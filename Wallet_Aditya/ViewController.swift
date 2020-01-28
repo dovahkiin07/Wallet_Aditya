3c50bed35857435115b7e7d56a9027200aa421c1
Adi <adityatuli01@gmail.com>
2020-01-28 18:24:07 -0500

    Initial Commit

---
+++ Wallet_Aditya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wallet_Aditya
+//
+//  Created by Aditya Tuli on 2020-01-28.
+//  Copyright © 2020 Aditya Tuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wallet_AdityaTests/Wallet_AdityaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wallet_AdityaTests.swift
+//  Wallet_AdityaTests
+//
+//  Created by Aditya Tuli on 2020-01-28.
+//  Copyright © 2020 Aditya Tuli. All rights reserved.
+//
+
+import XCTest
+@testable import Wallet_Aditya
+
+class Wallet_AdityaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
